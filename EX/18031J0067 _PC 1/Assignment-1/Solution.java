4827c885919e00a2a76ee5622913fd112f2d73dc
sushmitha9640 <sushmithasalandri@gmail.com>
2018-10-09 11:59:43 +0530

    pc1

--- EX/18031J0067 _PC 1/Assignment-1/Solution.java
+++ EX/18031J0067 _PC 1/Assignment-1/Solution.java
@@ -104,7 +104,7 @@ class list
 	}
 }
 class AddLargeNumbers {
-    static Scanner sc=new Scanner(System.in);
+    //static Scanner sc=new Scanner(System.in);
     public static list numberToDigits(String number) {
     	String s=number;
     	list one=new list();
@@ -113,7 +113,7 @@ class AddLargeNumbers {
     	{
     		int a=Character.getNumericValue(s.charAt(i));
     		one.add(a);
-    		
+    		//System.out.println(one.Disp());
     	}
     	//System.out.println(one.Disp());
 		return one;
@@ -139,7 +139,75 @@ class AddLargeNumbers {
 		}
 		//System.out.println("dis"+s);
 		return s;
+		
+    }
+
+    public static list addLargeNumbers(list pDigits, list qDigits) {
+    	int len1=pDigits.size;
+    	int len2=qDigits.size;
+    	int d=0;
+    	int e=0;
+    	list two=new list();
+    	if(len1>=len2)
+    	{
+
+    		for(int i=0;i<len1;i++)
+    		{
+    	int a=pDigits.popend();
+    	int b=qDigits.popend();
+    	
+    	int c=a+b+e;
+    
+    	if(i==len1-1)
+    	{
+    		if(e!=0)
+    		{
+    			two.addFirst(e);
+    		}
+    	}
+    	if(c>9)
+    	{
+    		d = c%10;
+    		e=c/10;
+    		two.addFirst(d);
+    	}
+    	else
+    	{
+    		d=0;
+    		e=0;
+    		two.addFirst(c);
+    	} 
+    	}
+    		return two;
+    	
+    		}
+		return two;
     }
 }
 
- 
+public class Solution1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String p = sc.nextLine();
+        String q = sc.nextLine();
+        switch(input){
+            case "numberToDigits":
+                list pDigits = AddLargeNumbers.numberToDigits(p);
+                System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
+                list qDigits = AddLargeNumbers.numberToDigits(q);
+               // System.out.println("....."+pDigits.Disp());
+                
+                System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
+                break;
+
+            case "addLargeNumbers":
+                list pDigits1 = AddLargeNumbers.numberToDigits(p);
+                list qDigits1 = AddLargeNumbers.numberToDigits(q);
+                list result = AddLargeNumbers.addLargeNumbers(pDigits1, qDigits1);
+                System.out.println(AddLargeNumbers.digitsToNumber(result));
+                break;
+        }
+    }
+    
+}
